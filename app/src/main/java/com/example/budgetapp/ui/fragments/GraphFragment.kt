70fb5edbd8a4f55cb66872ea39252bc704791c07
Vedran <vgrbavac@foi.hr>
2020-08-14 12:26:14 +0200

    Changed to better dependency for charts.

--- app/src/main/java/com/example/budgetapp/ui/fragments/GraphFragment.kt
+++ app/src/main/java/com/example/budgetapp/ui/fragments/GraphFragment.kt
@@ -5,54 +5,63 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.budgetapp.R
+import com.example.budgetapp.databinding.FragmentGraphBinding
+import com.example.budgetapp.viewmodels.TransactionsViewModel
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.fragment_graph.*
-import org.eazegraph.lib.models.PieModel
-
-
+import org.koin.android.ext.android.inject
 
 
 class GraphFragment : Fragment() {
+
+    private lateinit var binding: FragmentGraphBinding
+
+    var pieData: PieData? = null
+    var pieDataSet: PieDataSet? = null
+    private val pieEntries : MutableList<PieEntry> = arrayListOf()
+
+    private val viewModel: TransactionsViewModel by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_graph, container, false)
+        return binding.root
     }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        piechart.addPieSlice(
-            PieModel(
-                "R", 25f,
-                Color.parseColor("#FFA726")
-            )
-        )
-        piechart.addPieSlice(
-            PieModel(
-                "Python", 35f,
-                Color.parseColor("#66BB6A")
-            )
-        )
-        piechart.addPieSlice(
-            PieModel(
-                "C++", 15f,
-                Color.parseColor("#EF5350")
-            )
-        )
-        piechart.addPieSlice(
-            PieModel(
-                "Java", 25f,
-                Color.parseColor("#29B6F6")
-            )
-        )
 
-        piechart.startAnimation()
+        getEntries()
+        pieDataSet = PieDataSet(pieEntries, "")
+        pieData = PieData(pieDataSet)
+        pieChart.data = pieData
+        pieDataSet!!.setColors(*ColorTemplate.JOYFUL_COLORS)
+        pieDataSet!!.sliceSpace = 2f
+        pieDataSet!!.valueTextColor = Color.WHITE
+        pieDataSet!!.valueTextSize = 10f
+        pieDataSet!!.sliceSpace = 5f
+        pieChart.setUsePercentValues(true)
+        pieChart.animateXY(3000,3000)
     }
 
 
+    private fun getEntries() {
+        pieEntries.add(PieEntry(2f, "Food"))
+        pieEntries.add(PieEntry(4f, "Social life"))
+        pieEntries.add(PieEntry(6f, "Self development"))
+        pieEntries.add(PieEntry(8f, "Transportation"))
+        pieEntries.add(PieEntry(7f, "Education"))
+        pieEntries.add(PieEntry(3f, "Other"))
+    }
 }
